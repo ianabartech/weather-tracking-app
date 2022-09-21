e695cb9357bd8de40dde35072326124dd87780b8
Ian Garcia <113972003+ianabartech@users.noreply.github.com>
2022-09-21 11:56:14 +0800

    update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,18 +1,23 @@
 
 class Forecast{
 
+    //change and put your api key in this.key or you can also use dotenv
     constructor(){
         this.key = 'aBSA8FJWy9yXDzEFDlE379T6ymJXZ7rC';
         this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
         this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
 
     }
+
+    //This execute the two asynchronous function; getCity and getWeather.
     async updateCity(userInputCity){
         const cityFrom = await this.getCity(userInputCity);
         const weatherFrom = await this.getWeather(cityFrom.Key);
     
         return {cityFrom, weatherFrom};
     };
+
+    //This get the data from api, fetch it and get the response.
     async getCity(city){
         const query = `?apikey=${this.key}&q=${city}`;
         const response = await fetch(this.cityURI + query);
